e5401091fbe39b972a3ce4c9b49d345275f59a30
Andrey Soroko <andreysoroko84@gmail.com>
2023-02-25 19:27:36 +0300

    start

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    constructor() {
+        this.min;
+        this.mid;
+        this.max;
+      }
+      
+      
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+      
+      guess() {
+    
+        this.mid = Math.round(this.min + (this.max - this.min) / 2);
+        // console.log(`this.min: ${this.min} this.max: ${this.max} this.mid: ${this.mid}`);
+        return this.mid;
+    
+      }
+    
+      lower() {
+        this.max = this.mid;
+      }
+    
+      greater() {
+        this.min = this.mid;
+      }
 }
 
 module.exports = GuessingGame;
